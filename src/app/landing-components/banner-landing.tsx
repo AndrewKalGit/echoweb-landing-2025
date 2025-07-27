b1b30cafa7a5c9e0d39fdec8f169cee271f840e9
Andrew Kal <andrewkalgit@gmail.com>
2025-07-27 18:42:15 -0400

    MVP banner and service section

--- src/app/landing-components/banner-landing.tsx
+++ src/app/landing-components/banner-landing.tsx
@@ -24,10 +24,10 @@ const Bannerlanding = () => {
 		<section className='bg-gradient-to-br pt-4 bg-gray-300 via-white to-blue-50 lg:grid lg:place-content-center'>
 			<div className='bg-[url(/bannerbg.png)] bg-no-repeat bg-size-[820px] bg-right mx-auto min-h-[75vh] px-4 py-28 sm:px-32 sm:py-32'>
 				<div className='mx-auto text-center bg-white p-10 py-20 rounded-lg shadow-lg max-w-2xl'>
-					<h1 className='text-4xl sm:text-6xl font-bold text-gray-900 xl:text-7xl'>
+					<h1 className='text-3xl sm:text-4xl font-bold text-gray-900 xl:text-5xl'>
 						More <strong className='text-blue-500'>{buzzWord[0]}</strong>
 					</h1>
-					<h2 className='text-3xl sm:text-5xl font-bold text-gray-700 xl:text-6xl'>
+					<h2 className='text-3xl sm:text-4xl font-bold text-gray-700 xl:text-5xl'>
 						Less Stress
 					</h2>
 					<p className='mt-4 text-pretty text-gray-700 text-lg/relaxed xl:text-2xl/relaxed'>
---
+++ src/app/terms/page.tsx
@@ -0,0 +1,15 @@
+import Footer from '../landing-components/footer';
+import Header from '../landing-components/header';
+
+const page = () => {
+	return (
+		<div>
+			<Header></Header>
+			<h1 className='text-5xl font-semibold'>Terms and Conditions</h1>
+			<p></p>
+			<Footer></Footer>
+		</div>
+	);
+};
+
+export default page;
