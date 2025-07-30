2be05c72d0fc083defe365fb385f28e10d14b649
Andrew Kal <andrewkalgit@gmail.com>
2025-07-29 20:11:09 -0400

    privacy and terms mvp

--- src/app/landing-components/banner-landing.tsx
+++ src/app/landing-components/banner-landing.tsx
@@ -25,7 +25,10 @@ const Bannerlanding = () => {
 			<div className='bg-[url(/bannerbg.webp)] bg-no-repeat bg-size-[820px] bg-right mx-auto min-h-[75vh] px-4 py-28 sm:px-32 sm:py-32'>
 				<div className='mx-auto text-center bg-white p-10 py-20 rounded-lg shadow-lg max-w-2xl'>
 					<h1 className='text-3xl sm:text-4xl font-bold text-gray-900 xl:text-5xl'>
-						More <strong className='text-blue-500'>{buzzWord[0]}</strong>
+						More{' '}
+						<strong className='drop-shadow-2xl text-blue-500'>
+							{buzzWord[0]}
+						</strong>
 					</h1>
 					<h2 className='text-3xl sm:text-4xl font-bold text-gray-700 xl:text-5xl'>
 						Less Stress
--- src/app/landing-components/contact-section-landing.tsx
+++ src/app/landing-components/contact-section-landing.tsx
@@ -1,9 +1,38 @@
-const Contact = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import Image from 'next/image';
+import contactImg from '../../../public/contact.webp';
 
-export default Contact
+const Contact = () => {
+	return (
+		<section id='contact'>
+			<h1 className='flex justify-center text-5xl font-semibold mt-10 text-gray-800'>
+				Contact
+			</h1>
+			<div className='grid grid-cols-1 md:grid-cols-2 gap-8 max-w-screen-xl mx-auto p-8'>
+				<Image
+					src={contactImg}
+					alt='Visual Summary of Our Services'
+					className='rounded-lg w-full h-auto drop-shadow-2xl'
+				/>
+				<form className='shadow-lg mt-4 p-8 roudned-lg bg-white'>
+					<h2 className='text-2xl font-semibold mb-4 text-gray-800'>
+						More Questions?
+					</h2>
+					<label
+						htmlFor='name'
+						className='block text-sm font-medium text-gray-700'>
+						Name
+					</label>
+					<input
+						type='text'
+						id='name'
+						name='name'
+						required
+						className='mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm'
+					/>
+				</form>
+			</div>
+		</section>
+	);
+};
+
+export default Contact;
